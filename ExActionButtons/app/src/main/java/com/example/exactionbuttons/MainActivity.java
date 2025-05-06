62f28add7304c2c7a7655ee095834b3cf389a2eb
Laboratorio <felipesantilii@gmail.com>
2025-05-06 19:53:20 -0300

    Atividade 7

--- ExActionButtons/app/src/main/java/com/example/exactionbuttons/MainActivity.java
+++ ExActionButtons/app/src/main/java/com/example/exactionbuttons/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.exactionbuttons;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar myToolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.act_camera){
+            Toast.makeText(this, R.string.act_camera, Toast.LENGTH_SHORT).show();
+        }
+        if (item.getItemId() == R.id.act_call){
+            Toast.makeText(this, R.string.act_call, Toast.LENGTH_SHORT).show();
+        }
+        if (item.getItemId() == R.id.act_rotate){
+            Toast.makeText(this, R.string.act_rotate, Toast.LENGTH_SHORT).show();
+        }
+        if (item.getItemId() == R.id.act_help){
+            Toast.makeText(this, R.string.act_help, Toast.LENGTH_SHORT).show();
+        }
+        
+        return super.onOptionsItemSelected(item);
     }
 }
